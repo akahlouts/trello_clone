0f320029177318a7813ab13836edc0abda5f3da0
akahlouts <akahlouts@gmail.com>
2024-10-15 00:40:27 +0300

    add letter-spacing property to some classes

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,7 +6,9 @@ export const Logo = () => {
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
         <Image src="/logo.svg" alt="Logo" width={30} height={30} />
-        <p className="text-lg text-neutral-700 pb-1 font-bold">Taskify</p>
+        <p className="text-lg text-neutral-700 pb-1 font-bold tracking-[-1px]">
+          Taskify
+        </p>
       </div>
     </Link>
   );
