5c0d896abf520817726691d194ebf82a755a52fd
akahlouts <akahlouts@gmail.com>
2024-10-15 03:46:58 +0300

    implement createOrganiztionPage functionality

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -18,7 +18,7 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-2" />
           No 1 task management
         </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 font-bold tracking-[-3px]">
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 font-bold tracking-[-2px]">
           Taskify helps team move
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit font-bold tracking-[-3px]">
