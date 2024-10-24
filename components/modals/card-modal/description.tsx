e72651f51f9413bba62aa6fe3a9130674af07b73
akahlouts <akahlouts@gmail.com>
2024-10-24 16:39:01 +0300

    execute update card description function

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -7,6 +7,12 @@ import { useQueryClient } from "@tanstack/react-query";
 
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
+import { useAction } from "@/hooks/use-action";
+
+import { updateCard } from "@/actions/update-card";
+
+import { toast } from "sonner";
+
 import { AlignLeft } from "lucide-react";
 
 import { FormTextarea } from "@/components/form/form-textarea";
@@ -51,11 +57,24 @@ export const Description = ({ data }: DescriptionProps) => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["card", data.id] });
+
+      toast.success(`Card "${data.title}" updated`);
+
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const onSubmit = (formData: FormData) => {
     const description = formData.get("description") as string;
-    const { boardId } = params.boardId as string;
+    const boardId = params.boardId as string;
 
-    // TODO: Execute
+    execute({ id: data.id, description, boardId });
   };
 
   return (
@@ -64,12 +83,14 @@ export const Description = ({ data }: DescriptionProps) => {
       <div className="w-full">
         <p className="font-semibold text-neutral-700 mb-2">Description</p>
         {isEditing ? (
-          <form ref={formRef} className="space-y-2">
+          <form action={onSubmit} ref={formRef} className="space-y-2">
             <FormTextarea
               id="description"
               className="w-full mt-2"
               placeholder="Add a more detailed description"
               defaultValue={data.description || undefined}
+              errors={fieldErrors}
+              ref={textareaRef}
             />
             <div className="flex items-center gap-x-2">
               <FormSubmit>Save</FormSubmit>
